65f130484dbb9febecba211c5d814e835885148d
Kosztyk <36381705+Kosztyk@users.noreply.github.com>
2026-02-05 22:06:03 +0200

    Add files via upload

--- src/main/java/info/trizub/clamav/webclient/ClamAVWebClientApplication.java
+++ src/main/java/info/trizub/clamav/webclient/ClamAVWebClientApplication.java
@@ -1,1 +1,25 @@
-.
+/**
+ * Class Name: ClamAVWebClientApplication
+ * Description: This is the main class to run Spring MVC application.
+ * 
+ * Author: Ruslan Huzii
+ * Email:  ruslan@trizub.info
+ * Date: 2024-11-04
+ * 
+ * License: GNU Lesser General Public License v2.1
+ */
+package info.trizub.clamav.webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = "info.trizub.clamav.webclient")
+@SpringBootApplication
+public class ClamAVWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClamAVWebClientApplication.class, args);
+	}
+
+}
